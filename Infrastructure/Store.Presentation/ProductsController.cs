e2ddf95c550ede47a60ee8a4a779f0d1ff7eeacd
AhmedLotfy8 <ahmedfert45@gmail.com>
2025-10-24 16:02:31 +0200

    filtering using brand and type ids

--- Infrastructure/Store.Presentation/ProductsController.cs
+++ Infrastructure/Store.Presentation/ProductsController.cs
@@ -14,9 +14,9 @@ namespace Store.Presentation {
 
 
         [HttpGet]
-        public async Task<IActionResult> GetAllProducts() {
+        public async Task<IActionResult> GetAllProducts(int? brandId, int? typeId) {
 
-            var result = await _serviceManager.ProductService.GetAllProductsAsync();
+            var result = await _serviceManager.ProductService.GetAllProductsAsync(brandId, typeId);
 
             if (result is null) 
                 return BadRequest();
