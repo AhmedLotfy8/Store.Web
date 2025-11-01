9eb038064a411bd1025db5b1c2156a99f765093d
AhmedLotfy8 <ahmedfert45@gmail.com>
2025-11-01 16:20:37 +0200

    - Implementing global error handling middleware - Activating it in Program.cs

---
+++ Store.Web/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Store.Shared.ErrorModels;
+
+namespace Store.Web.Middlewares {
+    public class GlobalErrorHandlingMiddleware {
+
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandlingMiddleware(RequestDelegate next) {
+            _next = next;
+        }
+
+
+        public async Task InvokeAsync(HttpContext context) {
+
+            try {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex) {
+
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                context.Response.ContentType = "application/json";
+
+                var response = new ErrorDetails() {
+                    StatusCode = context.Response.StatusCode,
+                    ErrorMessage = ex.Message,
+                };
+
+                await context.Response.WriteAsJsonAsync(response);
+
+            }
+
+        }
+
+
+
+    }
+}
