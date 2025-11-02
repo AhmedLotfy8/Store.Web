9930978c332d692f07a7490abaa570e5a4fb041c
AhmedLotfy8 <ahmedfert45@gmail.com>
2025-11-02 18:20:02 +0200

    Basket module and repositroy implementation

---
+++ Core/Store.Domain/Entities/Baskets/CustomerBasket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities.Baskets {
+    public class CustomerBasket {
+   
+        
+        public string Id { get; set; }
+
+
+        public IEnumerable<BasketItem> Items { get; set; }
+
+
+    
+    }
+}
--- Infrastructure/Store.Persistence/InfrastructureServicesRegistration.cs
+++ Infrastructure/Store.Persistence/InfrastructureServicesRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 using Store.Domain.Contracts;
 using Store.Persistence.Data.Contexts;
 using System;
@@ -11,10 +12,10 @@ using System.Threading.Tasks;
 
 namespace Store.Persistence {
     public static class InfrastructureServicesRegistration {
-    
-    
+
+
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration) {
-            
+
             services.AddDbContext<StoreDbContext>(options => {
 
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
@@ -22,6 +23,12 @@ namespace Store.Persistence {
             });
             services.AddScoped<IDbInitializer, DbInitializer>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddSingleton<IConnectionMultiplexer>((ServiceProvider) =>
+
+                ConnectionMultiplexer.Connect(configuration.GetConnectionString("RedisConnection"))
+            );
+
 
             return services;
 
