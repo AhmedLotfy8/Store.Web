b82771076e8363ee01f01c471c2a0150d1894503
AhmedLotfy8 <ahmedfert45@gmail.com>
2025-11-11 03:15:40 +0200

    Order configurations

---
+++ Infrastructure/Store.Persistence/Data/Configurations/DeliveryMethodConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Domain.Entities.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Persistence.Data.Configurations {
+    public class DeliveryMethodConfigurations : IEntityTypeConfiguration<DeliveryMethod> {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder) {
+        
+            builder.Property(d => d.ShortName).HasColumnType("varchar").HasMaxLength(100);
+            builder.Property(d => d.Description).HasColumnType("varchar").HasMaxLength(256);
+            builder.Property(d => d.DeliveryTime).HasColumnType("varchar").HasMaxLength(128);
+            builder.Property(d => d.Price).HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
---
+++ Infrastructure/Store.Persistence/Data/Configurations/OrderItemConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Domain.Entities.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Persistence.Data.Configurations {
+    public class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem> {
+        public void Configure(EntityTypeBuilder<OrderItem> builder) {
+
+            builder.OwnsOne(oi => oi.Product);
+
+            builder.Property(oi => oi.Price).HasColumnType("decimal(18,2)");
+
+
+
+
+        }
+
+    }
+}
