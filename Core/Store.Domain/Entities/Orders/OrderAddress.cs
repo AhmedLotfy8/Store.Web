2a07b3f6438296ea32e8dfa81d02caf95b9faf4a
AhmedLotfy8 <ahmedfert45@gmail.com>
2025-11-11 02:52:16 +0200

    Order module models

---
+++ Core/Store.Domain/Entities/Orders/OrderAddress.cs
@@ -0,0 +1,16 @@
+﻿namespace Store.Domain.Entities.Orders {
+    public class OrderAddress {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+
+    }
+}
